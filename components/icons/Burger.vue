593a631f4f5c854eb2fffc95da64520a637b9ea5
Arthur FRIN <frin.arthur@gmail.com>
2024-09-08 18:21:08 +0200

    feat/nav bar

---
+++ components/icons/Burger.vue
@@ -0,0 +1,5 @@
+<template>
+    <svg width="29" height="26" viewBox="0 0 29 26" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0 0V3.625H29V0H0ZM0 10.7663V14.3913H29V10.7663H0ZM0 21.6413V25.2663H29V21.6413H0Z" />
+    </svg>
+</template>
--- components/navbar.vue
+++ components/navbar.vue
@@ -1,15 +1,38 @@
 <script lang="ts" setup>
+import Burger from './icons/Burger.vue';
+
+const burgerBool = ref(false);
+
+const toggleBurger = () => {
+    burgerBool.value = !burgerBool.value;
+}
+
 
 </script>
 
 <template>
-    <nav class="navbar">
-        <div class="container">
-            <h1 class="title">Planty</h1>
+    <div class="navbar">
+        <button class="burger" @click="toggleBurger()">
+            <Burger />
+        </button>
+        <nav class="navbar" v-if="burgerBool">
             <ul class="nav-links">
                 <li><a href="/">Home</a></li>
                 <li><a href="/plants">Plants</a></li>
             </ul>
-        </div>
-    </nav>
+        </nav>
+    </div>
 </template>
+
+<style lang="scss" scoped>
+.navbar {
+    position: absolute;
+    left: 1rem;
+    top: 1rem;
+
+}
+
+.burger {
+    fill: $icon-color;
+}
+</style>
